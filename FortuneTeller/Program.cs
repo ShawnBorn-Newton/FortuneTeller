4c1a002f34d5bb23a76cdd0d5cc370b4c949858e
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-06-30 11:41:31 -0400

    switch1

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -10,6 +10,55 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            //User Input and creat vars
+            string nameFirst;
+            string nameLast;
+            string responseColor;
+            int    responseAge;
+            double responseKin;
+
+            Console.WriteLine(" Welcome, let us peer through the currtens of fate!\n We shall explore your future useing special aspects of your life.");
+
+            Console.WriteLine(" To begine we must look at your name.\n Names are powerfull forces that shape fate and are shaped in turn.\n What is your first name?");
+                nameFirst = Console.ReadLine();
+            Console.WriteLine(" And your last name?");
+                nameLast = Console.ReadLine();
+            Console.WriteLine(" We must know how long you have tred your current path.\n How old are you?");
+                responseAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Color \"Help\"" );
+                responseColor = Console.ReadLine();
+            Console.WriteLine("Siblings");
+                responseKin = double.Parse(Console.ReadLine());
+
+            switch (responseColor)
+            {
+                case "red":
+
+                    break;
+                case "orange":
+
+                    break;
+                case "yellow":
+
+                    break;
+                case "Green":
+
+                    break;
+                case "blue":
+
+                    break;
+                case "indigo":
+
+                    break;
+                case "violet":
+
+                    break;
+            }
+
+            //use vars to put together fortune
+
+
+            //print fortune
         }
     }
 }
